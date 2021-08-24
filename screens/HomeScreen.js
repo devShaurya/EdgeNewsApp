7f6b4bbaaae59a863061de880ebbd2f39b2aab59
devShaurya <devshaurya1001@gmail.com>
2021-08-24 12:02:08 +0530

    removed console log

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -58,7 +58,7 @@ export default function HomeScreen({
                     setIsLoading(false);
                 }
             } catch (error) {
-                console.log(error);
+                // console.log(error);
                 setIsLoading(false);
             }
         }
